0b47fa076dd810fed37c5c03a2fb2389c11f95e4
Huu <vanhuudhsp@gmail.com>
2016-07-01 15:55:47 +0700

    Ionic App

---
+++ Ionic Blank App5/platforms/ios/www/js/app.js
@@ -0,0 +1,36 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(cordova.platformId === 'ios' && window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('view1', {
+            url: '/view1',
+            template: '<div class = "padding"> <h2>View 1</h2> <button class="button button-positive" ui-sref="view2">To view 2</button> </div>'
+        })
+        .state('view2', {
+            url: '/view2',
+            template: '<div class = "padding"> <h2>View 2</h2> <button class="button button-assertive" ui-sref="view1">To view 1</button> </div>'
+        })
+    $urlRouterProvider.otherwise('/view1')
+})
